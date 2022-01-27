e7db1b28d0058a4e64072d2cc63028cf51886e31
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2022-01-27 10:01:15 +0600

    Anonymous runnable++

--- src/main/java/github/com/passmesomesugar/anonymous_inner_class/Runner.java
+++ src/main/java/github/com/passmesomesugar/anonymous_inner_class/Runner.java
@@ -3,7 +3,6 @@ package github.com.passmesomesugar.anonymous_inner_class;
 public class Runner {
 
     public static void main(String[] args) {
-
         Animal animal = new Animal();
         animal.makeNoise();
 
@@ -14,5 +13,13 @@ public class Runner {
             }
         };
         bigFoot.makeNoise();
+
+        Runnable AnonRunnable = new Runnable() { /*This creates anonymous (with no name) subclass */
+            @Override
+            public void run() {
+                System.out.println("This is anonymous runnable");
+            }
+        };
+        AnonRunnable.run();
     }
 }
